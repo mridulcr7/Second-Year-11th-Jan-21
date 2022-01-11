1002bfe7965d47735092cd76afee7d4830d83c6a
mridulcr7 <83665684+mridulcr7@users.noreply.github.com>
2022-01-11 11:46:01 +0530

    Update Find the Running Median.cpp

--- Find the Running Median.cpp
+++ Find the Running Median.cpp
@@ -7,13 +7,38 @@ int main()
     int n,t,l=1;
     float c;
     cin>>n;
-    vector<int> a;
+    vector<int> a(n);
     vector<float>ans;
     for(int i=0;i<n;i++)
     {
         cin>>t;
-        a.push_back(t);
-        sort(a.begin(),a.end());
+       if(i==0)
+       {
+           a[i]=t;
+           ans.push_back(t);
+           l++;
+           continue;
+       }
+        int low=0,high=i,pos=0;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(t<a[mid])
+            {
+                low=mid+1;
+                pos=low;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        int j=i-1;
+         while (j >= pos) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j+1]=t;
         if(l%2!=0)
             ans.push_back(a[l/2]);
         else
